78fd13f6f4de41ff31b48534e2697500c41c2343
Thong Ha Minh <thong.haminh@floatingcube.com>
2026-05-13 18:32:43 +0700

    update

--- src/blocks/dw-features/index.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
-import Edit from './edit.js';
-import metadata from './block.json';
-
-registerBlockType(metadata.name, {
-  edit: Edit,
-});
